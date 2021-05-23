026f8ffb0a6c9f8f3b109025695d1d6f7918cff7
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-05-23 10:25:41 +0200

    Moved FieldEtsyCategory Moved SizeAndColorEnum Fixed AmazonEnum

---
+++ src/Fields/EtsyCategory.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\Etsy\Fields;
+
+use VitesseCms\Datafield\Models\Datafield;
+use VitesseCms\Database\AbstractCollection;
+use VitesseCms\Datafield\AbstractField;
+use VitesseCms\Form\AbstractForm;
+use VitesseCms\Form\Models\Attributes;
+use VitesseCms\Shop\Enums\SizeAndColorEnum;
+
+class EtsyCategory extends AbstractField
+{
+    public function buildItemFormElement(
+        AbstractForm $form,
+        Datafield $datafield,
+        Attributes $attributes,
+        AbstractCollection $data = null
+    )
+    {
+        $form->addText('Etsy CategoryId', 'etsyCategoryId')
+            ->addText('Etsy TaxanomyId', 'etsyTaxonomyId')
+            ->addText('Etsy Tags', 'etsyTags', (new Attributes())->setMultilang(true))
+            ->addText('Etsy SizeId', 'etsySizeId');
+
+        foreach (SizeAndColorEnum::sizes as $size => $sizeName) :
+            $form->addText($size . ' to Etsy Size', 'etsySizeMapper[' . $size . ']');
+        endforeach;
+    }
+}
