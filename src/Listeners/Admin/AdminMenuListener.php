a2bb125c6112b7c075e0dcb073b300ac4897a135
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-05-26 18:13:58 +0200

    Improved listeners

--- src/Listeners/Admin/AdminMenuListener.php
+++ src/Listeners/Admin/AdminMenuListener.php
@@ -5,15 +5,23 @@ namespace VitesseCms\Etsy\Listeners\Admin;
 use Phalcon\Events\Event;
 use VitesseCms\Admin\Models\AdminMenu;
 use VitesseCms\Admin\Models\AdminMenuNavBarChildren;
+use VitesseCms\Setting\Services\SettingService;
 
 class AdminMenuListener
 {
+    /**
+     * @var SettingService
+     */
+    private $settingService;
+
+    public function __construct(SettingService $settingService)
+    {
+        $this->settingService = $settingService;
+    }
+
     public function AddChildren(Event $event, AdminMenu $adminMenu): void
     {
-        if (
-            $adminMenu->getUser()->getPermissionRole() === 'superadmin'
-            && $adminMenu->getSetting()->has('ETSY_CONSUMER_KEY')
-        ) :
+        if ($this->settingService->has('ETSY_CONSUMER_KEY')) :
             $children = new AdminMenuNavBarChildren();
             $children->addChild('Raw Listing', 'admin/etsy/adminlisting/rawListingForm');
 
--- src/Listeners/InitiateAdminListeners.php
+++ src/Listeners/InitiateAdminListeners.php
@@ -2,13 +2,14 @@
 
 namespace VitesseCms\Etsy\Listeners;
 
-use Phalcon\Events\Manager;
+use VitesseCms\Core\Interfaces\InitiateListenersInterface;
+use VitesseCms\Core\Interfaces\InjectableInterface;
 use VitesseCms\Etsy\Listeners\Admin\AdminMenuListener;
 
-class InitiateAdminListeners
+class InitiateAdminListeners implements InitiateListenersInterface
 {
-    public static function setListeners(Manager $eventsManager): void
+    public static function setListeners(InjectableInterface $di): void
     {
-        $eventsManager->attach('adminMenu', new AdminMenuListener());
+        $di->eventsManager->attach('adminMenu', new AdminMenuListener($di->setting));
     }
 }
